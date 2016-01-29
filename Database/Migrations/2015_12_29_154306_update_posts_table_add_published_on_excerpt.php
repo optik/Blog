75eacd5cfabd27b58a10c066df8adb94d90ff505
optik <tomek@greymatter.pl>
2016-01-29 15:29:06 +0100

    Fixed migration name

--- Database/Migrations/2015_12_29_154306_update_posts_table_add_published_on_excerpt.php
+++ Database/Migrations/2015_12_29_154306_update_posts_table_add_published_on_excerpt.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdatePostsTable extends Migration {
+class UpdatePostsTableAddPublishedOnExcerpt extends Migration {
 
     /**
      * Run the migrations.
